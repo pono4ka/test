cb4b0389ce2fa95331aaf22e75336735e204cdc0
pono4ka <misterz@ziza.ru>
2013-09-22 19:09:12 +0600

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,2 +1,13 @@
-select * from dual; g g gggre
-rgbeberber
+undefined
+das dsa das a asd and I can type at  same time
+
+can u see me typing?
+I can
+:)
+great!
+
+The second problem is - there a twice more people here:)
+
+each of us have 2 persons here...dunno why yet
+
+I'm not very worried about #2.
